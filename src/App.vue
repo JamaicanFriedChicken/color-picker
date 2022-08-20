57269ec8c47ce432045301657d2df353d7d562b0
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2022-08-20 10:11:14 -0500

    remove color picker

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,19 @@
 <template>
   <div>
     <h8k-navbar v-bind:header="title"></h8k-navbar>
-    <ColorPicker
-        :color-picker-options="colorPickerOptions"
-        :initial-selected-color="initialSelectedColor"/>
+    <h1>Hello World</h1>
   </div>
 </template>
 
 <script>
-import ColorPicker from "@/components/ColorPicker";
 
 export default {
   components: {
-    ColorPicker
+
   },
   data() {
     return {
       title: 'Color Picker',
-      colorPickerOptions: ['#5d77f5', '#0fd085', '#ffba5b', '#f95e62', 'lightpink'],
-      initialSelectedColor: 'black'
     }
   }
 }
