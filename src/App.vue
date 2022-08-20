1bb3670786331739e18734c819b06f0476692978
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2022-08-20 12:14:15 -0500

    Revert "remove color picker"
    This reverts commit 57269ec8c47ce432045301657d2df353d7d562b0.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
     <h8k-navbar v-bind:header="title"></h8k-navbar>
-    <h1>Hello World</h1>
+    <ColorPicker
+        :color-picker-options="colorPickerOptions"
+        :initial-selected-color="initialSelectedColor"/>
   </div>
 </template>
 
 <script>
+import ColorPicker from "@/components/ColorPicker";
 
 export default {
   components: {
-
+    ColorPicker
   },
   data() {
     return {
       title: 'Color Picker',
+      colorPickerOptions: ['#5d77f5', '#0fd085', '#ffba5b', '#f95e62', 'lightpink'],
+      initialSelectedColor: 'black'
     }
   }
 }
